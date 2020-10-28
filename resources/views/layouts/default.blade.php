b77b589b2f1841ab19f9a8a6cbc73491dc964e4d
Rebirth-celebration <786742044@qq.com>
2020-10-28 05:57:30 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>NewWeibo App @yield('title','NewWeibo App')</title>
+</head>
+    <body>
+        @yield('content')
+    </body>
+</html>
