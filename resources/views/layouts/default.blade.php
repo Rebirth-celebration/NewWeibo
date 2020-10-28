3a2919b5332178d59a3d7d16d7085758848ca8d4
Rebirth-celebration <786742044@qq.com>
2020-10-28 07:09:21 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>NewWeibo App @yield('title','NewWeibo App')</title>
+    <link rel="stylesheet" href="css/app.css">
 </head>
-    <body>
-        @yield('content')
-    </body>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="">NewWeibo App</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a class="nav-link" href="">帮助</a></li>
+            <li class="nav-item"><a class="nav-link" href="">登录</a></li>
+        </ul>
+    </div>
+</nav>
+<div class="container">
+    @yield('content')
+</div>
+</body>
 </html>
