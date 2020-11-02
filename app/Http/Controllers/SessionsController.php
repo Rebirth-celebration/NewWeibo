871e5f2c6d55a68051dace36d7272e751b742778
Rebirth-celebration <786742044@qq.com>
2020-11-02 09:14:09 +0000

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -9,7 +9,7 @@ class SessionsController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('guest',[
+        $this->middleware('guest', [
             'only' => ['create']
         ]);
     }
@@ -26,12 +26,18 @@ class SessionsController extends Controller
             'password' => 'required',
         ]);
 
-        if (Auth::attempt($credentials,$request->has('remember'))) {
-            session()->flash('success','欢迎回来！');
-            $fallback =route('users.show',Auth::user());
-            return redirect()->intended($fallback);
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            if (Auth::user()->activated) {
+                session()->flash('success', '欢迎回来！');
+                $fallback = route('users.show', Auth::user());
+                return redirect()->intended($fallback);
+            } else {
+                Auth::logout();
+                session()->flash('warning','您的账号未激活，请检查邮箱中的注册邮件进行激活');
+                return redirect('/');
+            }
         } else {
-            session()->flash('danger','很抱歉，您的邮箱和密码不匹配！');
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配！');
             return redirect()->back()->withInput();
         }
     }
@@ -39,7 +45,7 @@ class SessionsController extends Controller
     public function destroy()
     {
         Auth::logout();
-        session()->flash('success','您已成功退出！');
+        session()->flash('success', '您已成功退出！');
         return redirect('login');
     }
 }
